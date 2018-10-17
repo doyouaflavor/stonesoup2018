eb6de3b07aac39293cb8ad571b2d7904961c07e3
Chang Shu-Huai <junsuwhy@gmail.com>
2018-10-17 17:10:42 +0800

    First commit.

---
+++ js/youtube_bg.js
@@ -0,0 +1,48 @@
+if(window.innerWidth > 768){
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('bg-video', {
+      height: '390',
+      width: '640',
+      videoId: 'c1fFiOM38-0',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      },
+      playerVars: {
+        disablekb: 1,
+        showinfo: 0, // deprecated after 25, sep, 2018
+        controls: 0,
+        rel: 0, // disabled after 25, sep, 2018
+        fs: 0
+      },
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    window.a = event;
+    event.target.playVideo();
+    event.target.mute();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  function onPlayerStateChange(event) {
+    // if (event.data == YT.PlayerState.PLAYING && !done) {
+    if (event.data == YT.PlayerState.ENDED) {
+      event.target.playVideo();
+    }
+  }
+  
+}
+
